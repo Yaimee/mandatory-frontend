08ae0aee4e4716568ac62710bf9ba4929065a6e9
R.Ø.M <rasmusorum@gmail.com>
2024-11-18 22:12:12 +0100

    change in vite version

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { render } from 'preact'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import { App } from './app.tsx'
 
-render(<App />, document.getElementById('app')!)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
